7caf8b42011bf96e3029133f748e56459519c307
michelle9193 <michellefortuin93@gmail.com>
2019-11-15 15:43:54 +0200

    create add dipslay book event

---
+++ app.js
@@ -0,0 +1,57 @@
+// Book Class: Represents a Book
+class Book {
+    constructor(title, author, isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+}
+
+// UI Class: Handle UI Tasks
+class UI {
+    static displayBooks() {
+        const storedBooks = [
+            {
+                title: 'Book One',
+                author: 'John Doe',
+                isbn: '123456'
+            },
+            {
+                title: 'Book Two',
+                author: 'Jane Doe',
+                isbn: '789456'
+            }
+        ];
+
+        const books = storedBooks;
+
+        books.forEach((book) => UI.addBookToList(book));
+    }
+
+    // Creating row to put into tbody with id book-list
+    static addBookToList(book) {
+        const list = document.querySelector('#book-list');
+        
+        // Create table row element tr
+        const row = document.createElement('tr');
+        
+        row.innerHTML = `
+            <td>${book.title}</td>
+            <td>${book.author}</td> 
+            <td>${book.isbn}</td>
+            <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+        `;
+
+        // Append row to list
+        list.appendChild(row);
+    }
+}
+
+// Store Class: Handles Storage
+
+// Event: Display Books
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
+
+// Event: Add Book
+
+// Event: Remove Book
