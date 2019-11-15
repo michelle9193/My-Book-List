c19b45ffd2239a8fcbc267031da0557b137357ae
michelle9193 <michellefortuin93@gmail.com>
2019-11-15 16:28:00 +0200

    create show/remove book

--- app.js
+++ app.js
@@ -52,12 +52,22 @@ class UI {
             }
         }
 
+        static showAlert(message, className) {
+            const div = document.createElement('div');
+            div.className = `alert alert-${className}`;
+            div.appendChild(document.createTextNode(message));
+            const container = document.querySelector('.container');
+            const form = document.querySelector('#book-form');
+            container.insertBefore(div, form);
+            // Vanish in 3 seconds
+            setTimeout(() => document.querySelector('.alert').remove(), 3000);
+        }
+
         static clearFields() {
             document.querySelector('#title').value='';
             document.querySelector('#author').value='';
             document.querySelector('#isbn').value='';
         }
-   
 }
 
 // Store Class: Handles Storage
@@ -75,17 +85,26 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
     const author = document.querySelector('#author').value;
     const isbn = document.querySelector('#isbn').value;
 
-    // Instatiate book 
-    const book = new Book(title, author, isbn);
+    // Validate
+    if(title === '' || author === '' || isbn === '') {
+        UI.showAlert('Please fill in all fields', 'danger');
+    } else {
+        // Instatiate book 
+        const book = new Book(title, author, isbn);
 
-    // Add Book to UI
-    UI.addBookToList(book);
+        // Add Book to UI
+        UI.addBookToList(book);
 
-    // Clear fields
-    UI.clearFields();
+        // Show success message
+        UI.showAlert('Book Added', 'success');
+        // Clear fields
+        UI.clearFields();
+    }   
 });
 
 // Event: Remove Book
 document.querySelector('#book-list').addEventListener('click', (e) => {
     UI.deleteBook(e.target)
 });
+ // Show removed message
+ UI.showAlert('Book Removed', 'success');
