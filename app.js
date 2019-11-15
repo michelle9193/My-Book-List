32dbb0c7791925ecefdc28be57bfe1461314c006
michelle9193 <michellefortuin93@gmail.com>
2019-11-15 15:54:59 +0200

    create add book to list

--- app.js
+++ app.js
@@ -53,5 +53,20 @@ class UI {
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
 // Event: Add Book
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+
+    // Prevent actual submit
+    e.preventDefault();
+    // Get form values
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const isbn = document.querySelector('#isbn').value;
+
+    // Instatiate book 
+    const book = new Book(title, author, isbn);
+
+    // Add Book to UI
+    UI.addBookToList(book);
+});
 
 // Event: Remove Book
